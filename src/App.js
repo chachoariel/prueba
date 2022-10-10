2e6c54cfd0e8e05c773d6445c6f1ab616c7ef54b
Alfredo Ariel Diaz <alfredodiazjjy@gmail.com>
2022-10-10 10:38:46 -0300

    clase 6 react router

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import Palettes from './components/Palette/Palettes';
-import Tags from './components/Tag/Tags';
-import Favorites from './components/Favorite/Favorites';
-import { getColorPalettes, getTags } from './service';
-import { FavoritesContext } from './context/FavoriteContext';
+	import {Routes, Route} from 'react-router-dom'
+	import Home from './routes/Home/Home';
+	import PaletteDisplay from './routes/Palette/PaletteDisplay';
+	import { useEffect, useState } from 'react';
+	import { getColorPalettes } from './service';
+	import { ColorPalettesContext} from './context/ColorPalettesContext'
 
-function App() {
-  const [colorPalettes, setColorPalettes] = useState([]);
-  const [tags, setTags] = useState([]);
-  const [favorites, setFavorites] = useState([]);
-
-  useEffect(() => {
-    getColorPalettes()
-      .then((data) => {
-        setColorPalettes(data);
-        setFavorites((data) => data.filter((palette) => palette.liked));
-      })
-      .catch((err) => console.log(err));
-
-    getTags()
-      .then((data) => setTags(data))
-      .catch((err) => console.log(err));
-  }, []);
-
-  return (
-    <FavoritesContext.Provider value={{favorites, setFavorites}}>
-      <div className='App'>
-        <header>
-          <h1>Color Palette Project</h1>
-        </header>
-        <div className='main-container'>
-          <Tags tags={tags} />
-          <Palettes palettes={colorPalettes} />
-          <Favorites favorites={favorites} />
-        </div>
-      </div>
-    </FavoritesContext.Provider>
-  );
-}
-
-export default App;
+  function App() {
+	  const [colorPalettes, setColorPalettes] = useState([]);
+	  useEffect(()=> {
+	    getColorPalettes()
+	    .then((data) => {
+	      setColorPalettes(data);
+	    })
+	    .catch((err) => console.log(err));
+	  }, [])
+	
+	  return (
+	      <div className='App'>
+	        <ColorPalettesContext.Provider value={{colorPalettes, setColorPalettes}}>
+	          <Routes> 
+	            <Route path='/' element={<Home/>}/>
+	            <Route path='/palette/:id' element={<PaletteDisplay/>}/>
+	          </Routes>
+	        </ColorPalettesContext.Provider>
+	      </div>
+	  );
+	}
+	
+	export default App;
